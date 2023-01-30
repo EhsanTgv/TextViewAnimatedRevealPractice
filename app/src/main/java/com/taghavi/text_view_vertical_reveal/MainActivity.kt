d179710e968e86fe7a6cb7800f22b7f2f48b9495
EhsanTgv <EhsanTgv@gmail.com>
2023-01-30 15:31:30 +0300

    Initial commit

---
+++ app/src/main/java/com/taghavi/text_view_vertical_reveal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taghavi.text_view_vertical_reveal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
